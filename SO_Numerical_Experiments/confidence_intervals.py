e6211eea363769d6a9bc4cd78130df24a2c948db
Schubydo <dylanschubert@rocketmail.com>
2024-07-04 16:43:44 +0100

    bo

--- SO_Numerical_Experiments/confidence_intervals.py
+++ SO_Numerical_Experiments/confidence_intervals.py
@@ -44,17 +44,24 @@ def compare_acquisition_functions(ucb, ucb_ci, ei, ei_ci, pi, pi_ci, filename='a
     final_pi = pi[-1]
     final_pi_ci = pi_ci[-1]
 
+    # Create comparisons and results
+    comparisons = [
+        ('UCB', final_ucb, final_ucb_ci, 'EI', final_ei, final_ei_ci),
+        ('UCB', final_ucb, final_ucb_ci, 'PI', final_pi, final_pi_ci),
+        ('EI', final_ei, final_ei_ci, 'UCB', final_ucb, final_ucb_ci),
+        ('EI', final_ei, final_ei_ci, 'PI', final_pi, final_pi_ci),
+        ('PI', final_pi, final_pi_ci, 'UCB', final_ucb, final_ucb_ci),
+        ('PI', final_pi, final_pi_ci, 'EI', final_ei, final_ei_ci),
+    ]
+
     results = []
-    for (acq1, final1, ci1), (acq2, final2, ci2) in [
-        (('UCB', final_ucb, final_ucb_ci), ('EI', final_ei, final_ei_ci)),
-        (('UCB', final_ucb, final_ucb_ci), ('PI', final_pi, final_pi_ci)),
-        (('EI', final_ei, final_ei_ci), ('PI', final_pi, final_pi_ci)),
-    ]:
-        best_acq = acq1 if final1 > final2 else acq2
-        superior = 'Yes' if final1 - ci1 > final2 + ci2 else 'No'
+    for acq1, final1, ci1, acq2, final2, ci2 in comparisons:
+        comparison_label = f'{acq1} > {acq2}'
+        is_greater = 1 if final1 > final2 else 0
+        superior = 1 if final1 - ci1 > final2 + ci2 else 0
         results.append({
-            'Comparison': f'{acq1} vs {acq2}',
-            'Best': best_acq,
+            'Comparison': comparison_label,
+            'Is_Greater': is_greater,
             'Superior': superior
         })
 
